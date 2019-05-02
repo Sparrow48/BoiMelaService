83482e4a8a89e4c0c59455466f25bb711ece9a58
IffatIslam15 <IffatIslam15@users.noreply.github.com>
2019-05-02 12:57:58 +0600

    registration.html file and registrationjs.js updated and new file registrationtestcss.css included

--- js/registrationjs.js
+++ js/registrationjs.js
@@ -1,44 +1,37 @@
-
 function formValidation()
 {
 var uid = document.registration.userid;
 var passid = document.registration.passid;
 var uname = document.registration.username;
+var uadd = document.registration.address;
 var uemail = document.registration.email;
-
-if(userid_validation(uid,5,12))
+ if(userid_validation(uid,5,12))
 {
 if(passid_validation(passid,7,12))
 {
 if(allLetter(uname))
 {
+if(alphanumeric(uadd))
+{ 
+if(countryselect(ucountry))
+{
+if(allnumeric(uzip))
+{
 if(ValidateEmail(uemail))
 {
+if(validsex(umsex,ufsex))
+{
 }
 } 
 }
 } 
-
+}
+}
+}
+}
 return false;
 
-} 
-
-function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
-} 
-
-function userid_validation(uid,mx,my)
+} function userid_validation(uid,mx,my)
 {
 var uid_len = uid.value.length;
 if (uid_len == 0 || uid_len >= my || uid_len < mx)
@@ -74,7 +67,77 @@ uname.focus();
 return false;
 }
 }
+function alphanumeric(uadd)
+{ 
+var letters = /^[0-9a-zA-Z]+$/;
+if(uadd.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('User address must have alphanumeric characters only');
+uadd.focus();
+return false;
+}
+}
+function countryselect(ucountry)
+{
+if(ucountry.value == "Default")
+{
+alert('Select your country from the list');
+ucountry.focus();
+return false;
+}
+else
+{
+return true;
+}
+}
+function allnumeric(uzip)
+{ 
+var numbers = /^[0-9]+$/;
+if(uzip.value.match(numbers))
+{
+return true;
+}
+else
+{
+alert('ZIP code must have numeric characters only');
+uzip.focus();
+return false;
+}
+}
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+} function validsex(umsex,ufsex)
+{
+x=0;
 
+if(umsex.checked) 
+{
+x++;
+} if(ufsex.checked)
+{
+x++; 
+}
+if(x==0)
+{
+alert('Select Male/Female');
+umsex.focus();
+return false;
+}
 else
 {
 alert('Form Succesfully Submitted');
@@ -82,4 +145,3 @@ window.location.reload()
 return true;
 }
 }
-
