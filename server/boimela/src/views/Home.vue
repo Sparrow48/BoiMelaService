acca4e09ebe07c6c4504e796ffec05d5deec329c
Nayeem M. Muzahid <nayeemm.muzahid@gmail.com>
2019-05-02 13:38:33 +0600

    boimela folder added

---
+++ server/boimela/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app >
+    <v-toolbar>
+    
+    <v-toolbar-title>বইমেলা</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      
+      <v-btn flat>Sign in</v-btn>
+      <v-btn flat>Sign Up</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+    
+  <v-toolbar>
+
+    <v-spacer></v-spacer>
+<v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Authors</v-btn>
+      <v-btn flat>Publishers</v-btn>
+      <v-btn flat>Book Fair 2019</v-btn>
+      <v-btn flat>Sign Up</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+ 
+}
+</script>
