6b48a609b00b191d74db75d1af91fe280e4044d7
Nasib48 <abunasib48@gmail.com>
2019-05-02 06:08:44 +0600

    Author.js added

---
+++ server/models/Author.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const authorSchema = new mongoose.Schema({ 
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    publishedBooks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book"
+    }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+});
+authorSchema.index({
+   "$**": "text"
+});
+
+module.exports = mongoose.model("Author", authorSchema);
