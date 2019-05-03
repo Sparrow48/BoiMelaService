ab3f1b92a18d7db8aea091218a8da405155203b9
Nayeem M. Muzahid <nayeemm.muzahid@gmail.com>
2019-05-04 03:18:15 +0600

    get method updated

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,33 +1,38 @@
-const mongoose = require("mongoose");
-const bookSchema = new mongoose.Schema({ 
-    bookName: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    picture: [{
-        type: String,
-        required: true
-    }],
-    authors: [{
-        type: String,
-        required: true
-    }],
-    authorUsername: [{
-        type: String,
-        require: true
-    }],
-    stallnumber:{
-        type: Number,
-        required: true
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+  bookName: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  picture: [
+    {
+      type: String,
+      required: true
     }
+  ],
+  authors: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  authorUsername: [
+    {
+      type: String,
+      require: true
+    }
+  ],
+  stallnumber: {
+    type: Number,
+    required: true
+  }
+});
+bookSchema.index({
+  '$**': 'text'
+});
 
-})
-  bookSchema.index({
-    "$**": "text"
-  });
-
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model('Book', bookSchema);
